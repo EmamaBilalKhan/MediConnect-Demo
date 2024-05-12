07df2fcee0b40ff3f188b75874c13c8ad50b81da
EmamaBilal <emamabilal.rao@gmail.com>
2024-05-12 19:29:53 +0500

    Updated Prescription Pattern

--- pages/Prescription.py
+++ pages/Prescription.py
@@ -17,7 +17,7 @@ st.header("Prescription")
 with open('Transcribe.txt', 'r') as file:
     default_text = file.read()
 
-response=get_gemini_response("Generate prescription from the following Patient Doctor Conversation, the pattern to follow for prescription is the standard prescription pattern of international doctors:"+default_text)
+response=get_gemini_response("Generate prescription from the following Patient Doctor Conversation, the pattern to follow should be standard pattern for prescription, it should include:Patient Name, Doctor Name, Date, Prescribed Medicine and dosage, strength, quantity refills and how to take and when to take for each medicine prescribed, doctor's signature, any recommended tests/treatments etc:"+default_text)
 st.write(response)
     
         
